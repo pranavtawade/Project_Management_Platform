a4cf4b26208479ff6e790fdd2d6ee0154e4fcf62
pranavtawade <tawade07pranav@gmail.com>
2025-12-23 23:33:49 +0530

    third commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import app from "./app.js"
+
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./.env",
+})
+
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
