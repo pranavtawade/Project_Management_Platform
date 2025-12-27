416ad4664ae0acfad13f432681e4f264ffedaeb4
pranavtawade <tawade07pranav@gmail.com>
2025-12-27 16:18:29 +0530

    11th-method to generate temporary hashed token with expiry

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
-
+import crypto from "crypto"
 
 const userSchema = new Schema (
     {
@@ -98,6 +98,17 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
+userSchema.methods.generateTemporaryToken = function (){
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
 
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000) //20 mins
+
+    return {unHashedToken, hashedToken, tokenExpiry}
+}
 
 export const User =  mongoose.model("User", userSchema)
